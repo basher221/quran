cab71429f15c1b63caf05ee2d280b53eb53158a1
basher123 <br5509150@gmail.com>
2026-02-24 06:06:27 +0300

    تحديث

--- www/script.js
+++ www/script.js
@@ -266,7 +266,7 @@ function initThemeMode() {
 function loadRecitationMode() {
   try {
     const storedMode = localStorage.getItem(RECITATION_MODE_STORAGE_KEY);
-    if (storedMode === "ayah" || storedMode === "surah") {
+    if (storedMode === "ayah") {
       return storedMode;
     }
   } catch (_error) {
@@ -288,6 +288,10 @@ function initRecitationMode() {
   state.recitationMode = initialMode;
   state.listen.playMode = initialMode;
   if (recitationModeSelect) {
+    const surahModeOption = recitationModeSelect.querySelector('option[value="surah"]');
+    if (surahModeOption) {
+      surahModeOption.disabled = true;
+    }
     recitationModeSelect.value = initialMode;
   }
   updateRecitationModeBadge();
@@ -3226,7 +3230,10 @@ if (themeModeSelect) {
 if (recitationModeSelect) {
   recitationModeSelect.addEventListener("change", (event) => {
     const selectedMode = event.target.value;
-    if (selectedMode !== "ayah" && selectedMode !== "surah") return;
+    if (selectedMode !== "ayah") {
+      recitationModeSelect.value = "ayah";
+      return;
+    }
 
     state.recitationMode = selectedMode;
     state.listen.playMode = selectedMode;
